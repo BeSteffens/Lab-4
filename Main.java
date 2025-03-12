85f0d4abbc46dcf2add312548d4fa7881ac36015
BeSteffens <S01286133@acad.tri-c.edu>
2025-03-12 19:32:38 +0000

    Done

--- Main.java
+++ Main.java
@@ -14,8 +14,8 @@ public class Main {
       Lab4.outputList(bubbleSortedList);
 
       System.out.println("\n\nInsertion sort results -------------------------------------------");
-      ArrayList<Integer> insertionSortedList = Lab4.bubbleSort(integerList);
-      Lab4.outputList(insertionSortedList);
+      ArrayList<Integer> insertionSortedList = Lab4.insertionSort(integerList);
+      Lab4.outputList(insertionSortedList); 
     }
 }
 
@@ -23,19 +23,36 @@ class Lab4 {
   public static ArrayList<Integer> insertionSort(ArrayList<Integer> integerList) {
     // Step 1 - Implement insertion sort algorithm here
 
-    /* for(int i = 1; i < integerList.length; i++){
-      int currentElement = integerList(i);
+    for(int i = 1; i < integerList.size(); i++){
+      int currentElement = integerList.get(i);
       int k;
-      for (k = i -1; k >= 0 && integerList(k) > currentElement; k--){
-       currentElement = integerList(k + 1);
+      for (k = i - 1; k >= 0 && integerList.get(k) > currentElement; k--){
+      integerList.set(k + 1, integerList.get(k));
       }
-    } */
-
+      integerList.set(k + 1,currentElement);
+    }
     return integerList;
   }
 
   public static ArrayList<Integer> bubbleSort(ArrayList<Integer> integerList) {
     // Step 2 - Implement the bubble sort algorithm here
+
+    boolean needPass = true;
+    int n = integerList.size();
+
+    for (int i = 0; i < n - 1 && needPass; i++){
+      needPass = false;
+      
+      for (int j = 0; j < n - 1 - i; j++){
+        if(integerList.get(j) > integerList.get(j + 1)){
+          
+          int temp = integerList.get(j);
+          integerList.set(j, integerList.get(j +1));
+          integerList.set(j +1, temp);
+          needPass = true;
+        }
+      }
+    }
 
     return integerList;
   }
